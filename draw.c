d9cb0be07f982b13c7c0e0ca414fc785c7d8e38b
jhua1 <jhua@stuy.edu>
2017-02-10 11:41:36 -0500

    line algorithm in draw.c

--- draw.c
+++ draw.c
@@ -7,5 +7,20 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int A,B,x,y,d;
+  x = x0;
+  y = y0;
+  A = y1 - y0;
+  B = x0 - x1;
+  d = B + 2*A;
+  while( x <= x1){
+    plot(s,c,x,y);
+    if ( d > 0){
+      y++;
+      d+=2*B;
+    }
+    x++;
+    d+=2*A;
+  }
 }
 
